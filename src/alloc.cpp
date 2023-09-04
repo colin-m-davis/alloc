186c2c79229d084a29daa88837bcc4991e911edf
colindavis <colinmichaelsdavis@gmail.com>
2023-09-04 13:43:50 -0700

    add private methods

--- src/alloc.cpp
+++ src/alloc.cpp
@@ -1,10 +1,15 @@
 #include <new>
+#include <optional>
+#include <unistd.h>
 
 using word_t = intptr_t;
 
-inline size_t align(size_t n) {
-  return (n + sizeof(intptr_t) - 1) & ~(sizeof(intptr_t) - 1);
-}
+struct Block {
+    size_t size;
+    bool used;
+    Block *next;
+    word_t data[1];
+};
 
 template <typename T>
 class Allocator {
@@ -18,12 +23,36 @@ public:
     template <typename U>
     Allocator(const Allocator<U>&) {}
 
-    pointer allocate(size_type n) {
+    auto allocate(size_type n) -> pointer {
+        auto block = request_from_os(n);
+
+        block->size = n;
+        block->used = true;
+
+        if (heap)
+        return block->data;
         return static_cast<pointer>(::operator new(n * sizeof(T)));
     }
 
-    void deallocate(pointer p, size_type) {
+    auto deallocate(pointer p, size_type) -> void {
         delete p;
+    }
+
+private:
+    auto request_from_os(size_type n) -> std::optional<Block*> {
+        auto block = static_cast<Block*>(sbrk(0)); // current heap break
+        if (sbrk(alloc_size(n)) == (void*)-1) {
+            return std::nullopt;
+        }
+        return std::optional(block);
+    }
+
+    auto alloc_size(size_type n) -> size_type {
+        return n + sizeof(Block) - sizeof(std::declval<Block>().data);
+    }
+
+    auto align(size_type n) -> size_type {
+        return (n + sizeof(intptr_t) - 1) & ~(sizeof(intptr_t) - 1);
     }
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,5 @@
 using namespace std;
 
 int main() {
-    for (size_t i = 0; i < 66; ++i) {
-        cout << i << " -> " << align(i) << '\n';
-    }
+    return 0;
 }
